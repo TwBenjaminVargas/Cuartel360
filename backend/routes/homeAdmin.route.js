46c474d4d7bc04b7a9d439087f9f43e661dba5f2
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-05-22 11:58:58 -0300

    Cambios de ruta

--- backend/routes/homeAdmin.route.js
+++ backend/routes/homeAdmin.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/registro', (req, res) => {
+router.get('/homeAdmin', (req, res) => {
     return res.sendFile(path.join(__dirname, '../../frontend/views/homeAdmin.html'));
 });
 
