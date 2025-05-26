d0f3e9117e87d9dd3e835cc1186b335933806531
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-05-26 12:38:44 -0300

    Calendario añadido al backend

--- backend/routes/api/api.calendar.route.js
+++ backend/routes/api/api.calendar.route.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
-const { Guardia } = require('../../models');
+const { Guardia, Bombero } = require('../../models');
 const router = express.Router();
 
 
 router.get('/api/guardias', async (req, res) => {
     const guardias = await Guardia.findAll();
-    res.json(guardias);
-});
+    const grilla = await Promise.all(guardias.map(async(guardia) => {
+        const bombero = await Bombero.findOne({ where: { id_bombero: guardia.id_bombero }});
+        return {
+            ...guardia.toJSON(), // convierte el modelo Sequelize a un objeto plano
+            title: bombero.nombre }
+        }));
 
+    res.json(grilla);
+
+});
 module.exports = router;
