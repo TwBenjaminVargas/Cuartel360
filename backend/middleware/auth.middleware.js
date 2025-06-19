99287c2b066e048ab7a74bf474eb08a68587983d
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-06-19 16:48:43 -0300

    redireccion a login ante token invalido

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,19 +1,25 @@
 const authService = require('../service/auth.service')
+
 const authMiddleware = (rol) => {
     return async (req, res, next) => {
-        try {
+        try 
+        {
             const token = req.cookies.token;
             const tokenPayload = await authService.validateToken(token);
             if(tokenPayload.rol > rol)
                 return res.status(401).json({ error: 'Rol no autorizado' });
             req.usuario = tokenPayload;
             next();
-        } catch (error) {
+        }
+        catch (error)
+        {
             // problemas con los tokens
-            console.log('Se notifico lo siguiente al cliente: \n',error.message);
-            return res.status(401).json({ error:error.message});
+            console.log('Middleware detecto: ',error.message);
+            return res.redirect('/');
         }
     };
 };
+
+
 
 module.exports = authMiddleware;
